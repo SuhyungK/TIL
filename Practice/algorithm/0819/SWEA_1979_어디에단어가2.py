b15e0f46b32cfa40f8e2e988df4ef5f364026986
SuhyungK <dook4966@gmail.com>
2022-08-19 17:56:00 +0900

    0819

--- Practice/algorithm/0819/SWEA_1979_어디에단어가2.py
+++ Practice/algorithm/0819/SWEA_1979_어디에단어가2.py
@@ -1,10 +1,12 @@
 import sys
 sys.stdin = open('input.txt')
 
-# T = int(input())
-# for tc in range(1,T+1):
-#     n, k = map(int, input().split())
-arr = [input() for _ in range(10)]
-print(arr)
+T = int(input())
+for tc in range(1,T+1):
+    n, k = map(int, input().split())
+    arr = [input().split('0') for _ in range(n)]
+    for row in arr:
+        for r in row:
+            print(r.strip())
 
     # print('#%d %d'%(tc))
