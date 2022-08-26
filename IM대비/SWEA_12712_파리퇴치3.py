0bfb9b06144287793d33475998402d7019f4e760
SuhyungK <dook4966@gmail.com>
2022-08-26 17:55:39 +0900

    0825

--- IM대비/SWEA_12712_파리퇴치3.py
+++ IM대비/SWEA_12712_파리퇴치3.py
@@ -6,17 +6,18 @@ for tc in range(1, T+1):
 
     for i in range(n):
         for j in range(n):
-            sumV = 0
+            sumV = arr[i][j]
             for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
-                for _ in range(m):
-                    if 0 <= nx < n and 0 <= nx < n : sumV += arr[nx][ny]
+                ni, nj = i + dx, j + dy
+                for _ in range(m-1):
+                    if 0 <= ni < n and 0 <= nj < n : sumV += arr[ni][nj]
                     else: break
                 if maxV < sumV: maxV = sumV
-            sumV = 0
+            sumV = arr[i][j]
             for dx, dy in ((1, 1), (-1, 1), (1, -1), (-1, -1)):
-                for _ in range(m):
+                for _ in range(m-1):
                     nx, ny = i + dx, j + dy
-                    if 0 <= nx < n and 0 <= nx < n: sumV += arr[nx][ny]
+                    if 0 <= nx < n and 0 <= ny < n: sumV += arr[nx][ny]
                 if maxV < sumV: maxV = sumV
     print(f'#%d'%tc)
     print(maxV)
