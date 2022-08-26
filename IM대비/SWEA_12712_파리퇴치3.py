02592ce94db934c7cf7db9c1ef8a4b93b7726aca
SuhyungK <dook4966@gmail.com>
2022-08-26 17:35:19 +0900

    0825

---
+++ IM대비/SWEA_12712_파리퇴치3.py
@@ -0,0 +1,10 @@
+T = int(input())
+for tc in range(1, T+1):
+    n, m = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+
+    for i in range(n):
+        for j in range(m):
+            for dx, dy in ((m, 0), ())
+    print(f'#%d'%tc)
+    print(arr)
