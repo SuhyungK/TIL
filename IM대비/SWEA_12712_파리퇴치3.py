f9f79959b68b1ee59e0d97ff8c9c238e4d152b14
SuhyungK <dook4966@gmail.com>
2022-08-26 17:39:02 +0900

    0825

--- IM대비/SWEA_12712_파리퇴치3.py
+++ IM대비/SWEA_12712_파리퇴치3.py
@@ -3,8 +3,12 @@ for tc in range(1, T+1):
     n, m = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(n)]
 
+
     for i in range(n):
-        for j in range(m):
-            for dx, dy in ((m, 0), ())
+        for j in range(n):
+            for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+                nx, ny = i + dx, j + dy
+                for _ in range(m):
+
     print(f'#%d'%tc)
     print(arr)
