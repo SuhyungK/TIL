78a4b5ef7f0513517c8ab4a60f8d8be803ca88ea
SuhyungK <dook4966@gmail.com>
2022-08-28 17:13:35 +0900

    0828

--- IM대비/SWEA_12712_파리퇴치3.py
+++ IM대비/SWEA_12712_파리퇴치3.py
@@ -6,18 +6,19 @@ for tc in range(1, T+1):
 
     for i in range(n):
         for j in range(n):
-            sumV = arr[i][j]
-            for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
-                ni, nj = i + dx, j + dy
-                for _ in range(m-1):
-                    if 0 <= ni < n and 0 <= nj < n : sumV += arr[ni][nj]
-                    else: break
-                if maxV < sumV: maxV = sumV
-            sumV = arr[i][j]
-            for dx, dy in ((1, 1), (-1, 1), (1, -1), (-1, -1)):
-                for _ in range(m-1):
-                    nx, ny = i + dx, j + dy
-                    if 0 <= nx < n and 0 <= ny < n: sumV += arr[nx][ny]
-                if maxV < sumV: maxV = sumV
-    print(f'#%d'%tc)
-    print(maxV)
+            sum1 = sum2 = arr[i][j]
+            for d in range(1,m):
+                for di, dj in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+                    ni, nj = i + di * d, j + dj * d
+                    if 0 <= ni < n and 0 <= nj < n:
+                        sum1 += arr[ni][nj]
+
+                for dx, dy in ((1, 1), (-1, 1), (1, -1), (-1, -1)):
+                    nx, ny = i + dx * d, j + dy * d
+                    if 0 <= nx < n and 0 <= ny < n:
+                        sum2 += arr[nx][ny]
+
+            cnt = max(sum1, sum2)
+            if maxV < cnt:
+                maxV = cnt
+    print(f'#{tc}', maxV)
